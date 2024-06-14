38dd2754da3db54371f23a21d08697ca91a7363b
Gabriel de Souza <gabriel.dsouzapro@gmail.com>
2024-06-14 16:57:20 -0300

    new realese
    Optimazed initial bundle size.
    Create floating navigation component.

---
+++ chunk-G3TVXW5J.js
@@ -0,0 +1,1 @@
+import{d as a,g as b}from"./chunk-VOXIISZH.js";import"./chunk-AVGM73E7.js";import"./chunk-ICTHBN6B.js";import"./chunk-SMBFLXJW.js";export{a as FooterComponent,b as NavigationComponent};
